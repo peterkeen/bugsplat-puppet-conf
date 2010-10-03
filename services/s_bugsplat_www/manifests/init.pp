b2c85c1599dc436e8bc770f15f48d81f8c94d8f2
Peter Keen <peter.keen@bugsplat.info>
2010-10-03 15:36:39 -0700

    whitespace woo!

--- services/s_bugsplat_www/manifests/init.pp
+++ services/s_bugsplat_www/manifests/init.pp
@@ -2,14 +2,14 @@ class s_bugsplat_www {
   include apache
 
   file { "/var/www/bugsplat.info" :
-    ensure => directory,
+    ensure  => directory,
     recurse => true,
-    owner => 'ubuntu',
+    owner   => 'ubuntu',
   }
 
   apache::vhost { "bugsplat.info":
     port => 80,
-    docroot => '/var/www/bugsplat.info',
+    docroot  => '/var/www/bugsplat.info',
     template => 's_bugsplat_www/vhost-default.conf.erb',
     priority => 10
   }
