b08a75c5db3de607caedc0cce5baa656f0167ce4
Peter Keen <peter.keen@bugsplat.info>
2010-10-03 17:51:26 -0700

    Add an explicit path to try to convince it to run a2enmod

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,6 +2,10 @@ Package {
   provider => aptitude
 }
 
+Exec {
+  path => "/usr/bin:/usr/sbin:/bin:/sbin"
+}
+
 node default {
   cron { puppet_update:
     command => "cd /etc/puppet && git pull origin master && puppet --modulepath /etc/puppet/modules:/etc/puppet/services --logdest syslog -v /etc/puppet/manifests/site.pp",
