39bd193cb50a016bad163bbf9d6fe59e926e9e04
Peter Keen <peter.keen@bugsplat.info>
2010-10-03 19:32:46 -0700

    add an explicit path, plus install apache2 before puppet runs the first time

--- tools/ec2-user-data.sh
+++ tools/ec2-user-data.sh
@@ -3,8 +3,11 @@
 set -e
 set -x
 
+export PATH="/sbin:/usr/sbin:/bin:/usr/bin:/usr/local/bin:/usr/local/sbin"
+
 apt-get -y install git-core
 apt-get -y install puppet
+apt-get -y install apache2
 
 rm -rf /etc/puppet
 
@@ -12,7 +15,7 @@ git clone git://github.com/peterkeen/bugsplat-puppet-conf /etc/puppet
 
 puppet_commad="puppet --modulepath /etc/puppet/modules:/etc/puppet/services --logdest syslog -v /etc/puppet/manifests/site.pp"
 
-$puppet_command
+$Puppet_command
 $puppet_command
 $puppet_command
 $puppet_command
